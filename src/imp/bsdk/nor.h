5f7a472a90c42057895ab239fe6578f0f2bc1a5c
ricynlee <ricyn@qq.com>
2021-10-28 22:38:16 +0800

    Update readme

--- src/imp/bsdk/nor.h
+++ src/imp/bsdk/nor.h
@@ -1,5 +1,5 @@
-#ifndef _BSP_NOR_H
-#define _BSP_NOR_H
+#ifndef _BSDK_NOR_H
+#define _BSDK_NOR_H
 
 #include "sdk.h"
 
@@ -8,4 +8,4 @@ extern void nor_erase(size_t start, size_t size);
 extern void nor_program(size_t start, size_t size, const uint8_t* const data);
 extern void nor_read(size_t start, size_t size, uint8_t* const data);
 
-#endif // _BSP_NOR_H
+#endif // _BSDK_NOR_H
