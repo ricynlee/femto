0cf905a693726b3fa29f7940071bd564d9b97fca
ricynlee <ricyn@qq.com>
2021-11-21 11:47:46 +0800

    Breathing LED indicator for uart boot

--- src/imp/rom/main.c
+++ src/imp/rom/main.c
@@ -1,16 +1,60 @@
 #include "bsdk.h"
 
-const uint8_t wdata[] = "This is a test";
-uint8_t rdata[sizeof(wdata)];
+void go_uart_boot(void);
+void go_nor_boot(void);
 
-void main() {
+void main(void) {
+    // wait 16 sec for uart activity
     gpio_init();
-    nor_bus_read_init();
-    light_leds(true, false, false);
-    nor_erase_block(0u);
-    light_leds(false, true, false);
-    nor_program(0u, wdata, sizeof(wdata));
-    light_leds(false, false, true);
-    nor_read(0u, rdata, sizeof(wdata));
+    uart_clear_rx_fifo();
+
+    for (int t=0; t<8; t++) {
+        for (int dc=0; dc<64; dc++) {
+            for (int c=0; c<64; c++) {
+                timer_set(256u);
+                while (timer_get())
+                    if (uart_rx_ready())
+                        goto done_waiting;
+                light_leds(false, c<dc, false);
+            }
+        }
+
+        for (int dc=0; dc<64; dc++) {
+            for (int c=0; c<64; c++) {
+                timer_set(256u);
+                while (timer_get())
+                    if (uart_rx_ready())
+                        goto done_waiting;
+                light_leds(false, c>=dc, false);
+            }
+        }
+    }
+
+    done_waiting:
+
+    // led off
+    timer_set(128u); // do not cause a glitch
+    while (timer_get());
+    light_leds(false, false, false);
+
+    // boot flow
+    if (uart_rx_ready()) {
+        go_uart_boot();
+    } else {
+        go_nor_boot();
+    }
+
+    // should never arrive here
+    while(1);
+}
+
+void go_uart_boot(void) {
+    // TODO
+
+}
+
+void go_nor_boot(void) {
+    void (*func)(void) = (void (*)(void))(NOR);
+    func();
     while(1);
 }
