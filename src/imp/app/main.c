fe496fc7adcdc4be7ea9711ec89bb33d88895112
ricynlee <ricyn@qq.com>
2021-11-27 23:48:27 +0800

    - Fix NOR linker script - Calib delay in app

--- src/imp/app/main.c
+++ src/imp/app/main.c
@@ -9,7 +9,7 @@ void main(void) {
         for (int t=0; t<2; t++) {
             for (int dc=0; dc<64; dc++)
                 for (int c=0; c<64; c++) {
-                    timer_delay_us(256u);
+                    timer_delay_us(192u); // looks like NOR is too slow to use 256
                     light_leds(false, false, (t & 0x1u) ? (c>dc) : (c<=dc));
                 }
             if (t & 0x1u) {
